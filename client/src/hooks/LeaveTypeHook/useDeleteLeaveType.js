871b9899f92e138d2a57b5f07dea02e22a2b2ebd
johnjoel061 <94114474+johnjoel061@users.noreply.github.com>
2024-10-01 10:33:06 +0800

    Update useDeleteLeaveType.js

--- client/src/hooks/LeaveTypeHook/useDeleteLeaveType.js
+++ client/src/hooks/LeaveTypeHook/useDeleteLeaveType.js
@@ -11,7 +11,7 @@ const useDeleteLeaveTypeById = () => {
     setLoading(true);
     setError(null);
     try {
-      const response = await axios.delete(`http://localhost:3000/api/leave-type/${id}`, {
+      const response = await axios.delete(`https://hr-management-1-baxp.onrender.com/api/leave-type/${id}`, {
         headers: {
           'Content-Type': 'application/json',
         }
