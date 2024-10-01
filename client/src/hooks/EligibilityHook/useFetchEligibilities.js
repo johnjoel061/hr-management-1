58f7c584da8c505ff1bb0270732cf9300db1f2f5
johnjoel061 <94114474+johnjoel061@users.noreply.github.com>
2024-10-01 10:22:37 +0800

    Update useFetchEligibilities.js

--- client/src/hooks/EligibilityHook/useFetchEligibilities.js
+++ client/src/hooks/EligibilityHook/useFetchEligibilities.js
@@ -9,7 +9,7 @@ const useFetchEligibilities = () => {
   const fetchEligibilities = useCallback(async () => {
     setLoading(true); // Ensure loading state is true when refetching
     try {
-      const response = await axios.get('http://localhost:3000/api/eligibility/all', {
+      const response = await axios.get('https://hr-management-1-baxp.onrender.com/api/eligibility/all', {
         headers: {
           Authorization: `Bearer ${localStorage.getItem('token')}`
         }
