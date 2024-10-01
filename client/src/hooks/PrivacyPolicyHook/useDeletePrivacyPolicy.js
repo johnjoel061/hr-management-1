07013a53ffb9c9ebef3d6d550ed2f460b3cfefa3
johnjoel061 <94114474+johnjoel061@users.noreply.github.com>
2024-10-01 10:38:38 +0800

    Update useDeletePrivacyPolicy.js

--- client/src/hooks/PrivacyPolicyHook/useDeletePrivacyPolicy.js
+++ client/src/hooks/PrivacyPolicyHook/useDeletePrivacyPolicy.js
@@ -11,7 +11,7 @@ const useDeletePrivacyPolicy = () => {
     setLoading(true);
     setError(null);
     try {
-      const response = await axios.delete(`http://localhost:3000/api/privacy-policy/${id}`, {
+      const response = await axios.delete(`https://hr-management-1-baxp.onrender.com/api/privacy-policy/${id}`, {
         headers: {
           'Content-Type': 'application/json',
         }
