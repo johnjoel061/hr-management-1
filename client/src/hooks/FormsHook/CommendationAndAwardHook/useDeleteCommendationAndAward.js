47837507a5609cbe7f7fa39ae4d983bb444c3cad
johnjoel061 <94114474+johnjoel061@users.noreply.github.com>
2024-10-01 10:59:54 +0800

    Update useDeleteCommendationAndAward.js

--- client/src/hooks/FormsHook/CommendationAndAwardHook/useDeleteCommendationAndAward.js
+++ client/src/hooks/FormsHook/CommendationAndAwardHook/useDeleteCommendationAndAward.js
@@ -11,7 +11,7 @@ const useDeleteCommendationAndAwardById = () => {
     setLoading(true);
     setError(null);
     try {
-      const response = await axios.delete(`http://localhost:3000/api/commendations-and-awards/${id}`, {
+      const response = await axios.delete(`https://hr-management-1-baxp.onrender.com/api/commendations-and-awards/${id}`, {
         headers: {
           'Content-Type': 'application/json',
         }
