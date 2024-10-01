6779d03374c30e47833a3272d4ddd7ff33dd1917
johnjoel061 <94114474+johnjoel061@users.noreply.github.com>
2024-10-01 10:09:05 +0800

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import commonjs from '@rollup/plugin-commonjs';
 
 export default defineConfig({
   plugins: [
     react(),
-    commonjs(),
   ],
 });
