3e1a74a7813ee6ec541dc5ff61e70c353be00fb3
johnjoel061 <94114474+johnjoel061@users.noreply.github.com>
2024-10-01 10:57:00 +0800

    Update useFetchCertificateOfEligibility.js

--- client/src/hooks/FormsHook/CertificateOfEligibilityHook/useFetchCertificateOfEligibility.js
+++ client/src/hooks/FormsHook/CertificateOfEligibilityHook/useFetchCertificateOfEligibility.js
@@ -9,7 +9,7 @@ const useFetchCertificateOfEligibility = () => {
   const fetchCertificateOfEligibility = useCallback(async () => {
     setLoading(true); // Ensure loading state is true when refetching
     try {
-      const response = await axios.get('http://localhost:3000/api/certificate-of-eligibility/all', {
+      const response = await axios.get('https://hr-management-1-baxp.onrender.com/api/certificate-of-eligibility/all', {
         headers: {
           Authorization: `Bearer ${localStorage.getItem('token')}`
         }
