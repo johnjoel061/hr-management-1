9e2ae95b0ae5efea64424f07105121add142c2cf
johnjoel061 <94114474+johnjoel061@users.noreply.github.com>
2024-10-01 10:49:07 +0800

    Update useFetchAppointment.js

--- client/src/hooks/FormsHook/AppointmentHook/useFetchAppointment.js
+++ client/src/hooks/FormsHook/AppointmentHook/useFetchAppointment.js
@@ -9,7 +9,7 @@ const useFetchAppointment = () => {
   const fetchAppointment = useCallback(async () => {
     setLoading(true); // Ensure loading state is true when refetching
     try {
-      const response = await axios.get('http://localhost:3000/api/appointment/all', {
+      const response = await axios.get('https://hr-management-1-baxp.onrender.com/api/appointment/all', {
         headers: {
           Authorization: `Bearer ${localStorage.getItem('token')}`
         }
