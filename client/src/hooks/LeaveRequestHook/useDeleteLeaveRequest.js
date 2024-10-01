7952b36e8a41a697b1fc8b2a7887740e9518ab01
johnjoel061 <94114474+johnjoel061@users.noreply.github.com>
2024-10-01 10:31:07 +0800

    Update useDeleteLeaveRequest.js

--- client/src/hooks/LeaveRequestHook/useDeleteLeaveRequest.js
+++ client/src/hooks/LeaveRequestHook/useDeleteLeaveRequest.js
@@ -11,7 +11,7 @@ const useDeleteLeaveRequest = () => {
     setLoading(true);
     setError(null);
     try {
-      const response = await axios.delete(`http://localhost:3000/api/employee/leave-requests/${leaveRequestId}`, {
+      const response = await axios.delete(`https://hr-management-1-baxp.onrender.com/api/employee/leave-requests/${leaveRequestId}`, {
         headers: {
           Authorization: `Bearer ${localStorage.getItem('token')}`,
           'Content-Type': 'application/json',
