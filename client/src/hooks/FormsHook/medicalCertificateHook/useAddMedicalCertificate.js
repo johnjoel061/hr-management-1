81dc02af6f34b2b8710bebca897b23fa86900b21
johnjoel061 <94114474+johnjoel061@users.noreply.github.com>
2024-10-01 11:19:57 +0800

    Update useAddMedicalCertificate.js

--- client/src/hooks/FormsHook/medicalCertificateHook/useAddMedicalCertificate.js
+++ client/src/hooks/FormsHook/medicalCertificateHook/useAddMedicalCertificate.js
@@ -21,7 +21,7 @@ const useAddMedicalCertificate = () => {
       });
 
       // Make API request to add personal data sheet
-      const response = await axios.post('http://localhost:3000/api/medical-certificate/add', formData, {
+      const response = await axios.post('https://hr-management-1-baxp.onrender.com/api/medical-certificate/add', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
