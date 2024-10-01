5c3b62481f0b8e17526f146b4b7186ebb7b4bb81
johnjoel061 <94114474+johnjoel061@users.noreply.github.com>
2024-10-01 11:01:46 +0800

    Update useAddCopiesOfDiscipAction.js

--- client/src/hooks/FormsHook/CopiesOfDiscipActionHook/useAddCopiesOfDiscipAction.js
+++ client/src/hooks/FormsHook/CopiesOfDiscipActionHook/useAddCopiesOfDiscipAction.js
@@ -21,7 +21,7 @@ const useAddCopiesOfDiscipActionHook = () => {
       });
 
       // Make API request to add personal data sheet
-      const response = await axios.post('http://localhost:3000/api/copies-of-disciplinary-actions/add', formData, {
+      const response = await axios.post('https://hr-management-1-baxp.onrender.com/api/copies-of-disciplinary-actions/add', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
