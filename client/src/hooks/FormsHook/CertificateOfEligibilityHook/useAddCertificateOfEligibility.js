2d45c14aa19d5c25c79c9014480b72bd17f8e652
johnjoel061 <94114474+johnjoel061@users.noreply.github.com>
2024-10-01 10:54:38 +0800

    Update useAddCertificateOfEligibility.js

--- client/src/hooks/FormsHook/CertificateOfEligibilityHook/useAddCertificateOfEligibility.js
+++ client/src/hooks/FormsHook/CertificateOfEligibilityHook/useAddCertificateOfEligibility.js
@@ -21,7 +21,7 @@ const useAddCertificateOfEligibility = () => {
       });
 
       // Make API request to add personal data sheet
-      const response = await axios.post('http://localhost:3000/api/certificate-of-eligibility/add', formData, {
+      const response = await axios.post('https://hr-management-1-baxp.onrender.com/api/certificate-of-eligibility/add', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
