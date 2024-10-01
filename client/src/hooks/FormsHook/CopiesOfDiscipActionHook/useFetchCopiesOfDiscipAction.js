aa35056e0e1e37f05ab1f705368ae215abc3db34
johnjoel061 <94114474+johnjoel061@users.noreply.github.com>
2024-10-01 11:02:23 +0800

    Update useFetchCopiesOfDiscipAction.js

--- client/src/hooks/FormsHook/CopiesOfDiscipActionHook/useFetchCopiesOfDiscipAction.js
+++ client/src/hooks/FormsHook/CopiesOfDiscipActionHook/useFetchCopiesOfDiscipAction.js
@@ -9,7 +9,7 @@ const useFetchCopiesOfDiscipAction = () => {
   const fetchCopiesOfDiscipAction = useCallback(async () => {
     setLoading(true); // Ensure loading state is true when refetching
     try {
-      const response = await axios.get('http://localhost:3000/api/copies-of-disciplinary-actions/all', {
+      const response = await axios.get('https://hr-management-1-baxp.onrender.com/api/copies-of-disciplinary-actions/all', {
         headers: {
           Authorization: `Bearer ${localStorage.getItem('token')}`
         }
