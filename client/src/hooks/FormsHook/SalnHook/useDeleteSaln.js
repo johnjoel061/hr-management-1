c3f7eb5491c70a5f7091853fe08433a690dc7f1e
johnjoel061 <94114474+johnjoel061@users.noreply.github.com>
2024-10-01 11:17:07 +0800

    Update useDeleteSaln.js

--- client/src/hooks/FormsHook/SalnHook/useDeleteSaln.js
+++ client/src/hooks/FormsHook/SalnHook/useDeleteSaln.js
@@ -11,7 +11,7 @@ const useDeleteSalnById = () => {
     setLoading(true);
     setError(null);
     try {
-      const response = await axios.delete(`http://localhost:3000/api/saln/${id}`, {
+      const response = await axios.delete(`https://hr-management-1-baxp.onrender.com/api/saln/${id}`, {
         headers: {
           'Content-Type': 'application/json',
         }
