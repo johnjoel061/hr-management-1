fb4124e8b278a25ed8cd66ab80b32d1b5f5a2588
johnjoel061 <94114474+johnjoel061@users.noreply.github.com>
2024-10-01 10:58:01 +0800

    Update useDeleteClearanceMoneyPropertyAcctHook.js

--- client/src/hooks/FormsHook/ClearanceMoneyPropertyAcctHook/useDeleteClearanceMoneyPropertyAcctHook.js
+++ client/src/hooks/FormsHook/ClearanceMoneyPropertyAcctHook/useDeleteClearanceMoneyPropertyAcctHook.js
@@ -11,7 +11,7 @@ const useDeleteClearanceMoneyPropertyAcctById = () => {
     setLoading(true);
     setError(null);
     try {
-      const response = await axios.delete(`http://localhost:3000/api/clearance-from-money-and-property-accountabilities/${id}`, {
+      const response = await axios.delete(`https://hr-management-1-baxp.onrender.com/api/clearance-from-money-and-property-accountabilities/${id}`, {
         headers: {
           'Content-Type': 'application/json',
         }
