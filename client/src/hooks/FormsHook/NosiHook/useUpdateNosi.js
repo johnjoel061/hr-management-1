52b0206a7c99d9cd12c4da69b3e0c9605db92be4
johnjoel061 <94114474+johnjoel061@users.noreply.github.com>
2024-10-01 11:11:24 +0800

    Update useUpdateNosi.js

--- client/src/hooks/FormsHook/NosiHook/useUpdateNosi.js
+++ client/src/hooks/FormsHook/NosiHook/useUpdateNosi.js
@@ -28,7 +28,7 @@ const useUpdateNosiById = () => {
         }
       });
 
-      const response = await axios.put(`http://localhost:3000/api/nosi/${id}`, formData, {
+      const response = await axios.put(`https://hr-management-1-baxp.onrender.com/api/nosi/${id}`, formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
           Authorization: `Bearer ${localStorage.getItem('token')}`,
