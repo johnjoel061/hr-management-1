311a5a056829e2908a111bb5ccd585d0d416214f
johnjoel061 <94114474+johnjoel061@users.noreply.github.com>
2024-10-01 11:20:33 +0800

    Update useFetchMedicalCertificate.js

--- client/src/hooks/FormsHook/medicalCertificateHook/useFetchMedicalCertificate.js
+++ client/src/hooks/FormsHook/medicalCertificateHook/useFetchMedicalCertificate.js
@@ -9,7 +9,7 @@ const useFetchMedicalCertificate = () => {
   const fetchMedicalCertificate = useCallback(async () => {
     setLoading(true); // Ensure loading state is true when refetching
     try {
-      const response = await axios.get('http://localhost:3000/api/medical-certificate/all', {
+      const response = await axios.get('https://hr-management-1-baxp.onrender.com/api/medical-certificate/all', {
         headers: {
           Authorization: `Bearer ${localStorage.getItem('token')}`
         }
