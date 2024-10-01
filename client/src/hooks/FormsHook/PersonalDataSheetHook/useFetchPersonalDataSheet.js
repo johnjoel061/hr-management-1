6ee30f39f33c20375b0c8c5bbaa83b031544c5b3
johnjoel061 <94114474+johnjoel061@users.noreply.github.com>
2024-10-01 11:14:35 +0800

    Update useFetchPersonalDataSheet.js

--- client/src/hooks/FormsHook/PersonalDataSheetHook/useFetchPersonalDataSheet.js
+++ client/src/hooks/FormsHook/PersonalDataSheetHook/useFetchPersonalDataSheet.js
@@ -9,7 +9,7 @@ const useFetchPersonalDataSheet = () => {
   const fetchPersonalDataSheet = useCallback(async () => {
     setLoading(true); // Ensure loading state is true when refetching
     try {
-      const response = await axios.get('http://localhost:3000/api/personal-data-sheet/all', {
+      const response = await axios.get('https://hr-management-1-baxp.onrender.com/api/personal-data-sheet/all', {
         headers: {
           Authorization: `Bearer ${localStorage.getItem('token')}`
         }
