91b91dc9a13fcb3c090dfc585d123fad7e64b475
johnjoel061 <94114474+johnjoel061@users.noreply.github.com>
2024-10-01 11:20:17 +0800

    Update useDeleteMedicalCertificate.js

--- client/src/hooks/FormsHook/medicalCertificateHook/useDeleteMedicalCertificate.js
+++ client/src/hooks/FormsHook/medicalCertificateHook/useDeleteMedicalCertificate.js
@@ -11,7 +11,7 @@ const useDeleteMedicalCertificateById = () => {
     setLoading(true);
     setError(null);
     try {
-      const response = await axios.delete(`http://localhost:3000/api/medical-certificate/${id}`, {
+      const response = await axios.delete(`https://hr-management-1-baxp.onrender.com/api/medical-certificate/${id}`, {
         headers: {
           'Content-Type': 'application/json',
         }
