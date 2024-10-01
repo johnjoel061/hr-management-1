3127faaadb25235a86f15dd739dda41b3e02f610
johnjoel061 <94114474+johnjoel061@users.noreply.github.com>
2024-10-01 11:03:25 +0800

    Update useDeleteCos.js

--- client/src/hooks/FormsHook/CosHook/useDeleteCos.js
+++ client/src/hooks/FormsHook/CosHook/useDeleteCos.js
@@ -11,7 +11,7 @@ const useDeleteCosById = () => {
     setLoading(true);
     setError(null);
     try {
-      const response = await axios.delete(`http://localhost:3000/api/contract-of-service/${id}`, {
+      const response = await axios.delete(`https://hr-management-1-baxp.onrender.com/api/contract-of-service/${id}`, {
         headers: {
           'Content-Type': 'application/json',
         }
