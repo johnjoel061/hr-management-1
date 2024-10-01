b6b340fe5c75640d0a8c1bb423539132941e3822
johnjoel061 <94114474+johnjoel061@users.noreply.github.com>
2024-10-01 11:05:01 +0800

    Update useFetchDesignation.js

--- client/src/hooks/FormsHook/DesignationHook/useFetchDesignation.js
+++ client/src/hooks/FormsHook/DesignationHook/useFetchDesignation.js
@@ -9,7 +9,7 @@ const useFetchDesignation = () => {
   const fetchDesignation = useCallback(async () => {
     setLoading(true); // Ensure loading state is true when refetching
     try {
-      const response = await axios.get('http://localhost:3000/api/designation/all', {
+      const response = await axios.get('https://hr-management-1-baxp.onrender.com/api/designation/all', {
         headers: {
           Authorization: `Bearer ${localStorage.getItem('token')}`
         }
