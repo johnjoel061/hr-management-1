f79d990b2bb1ecdca00afcf002a4203279a4f253
johnjoel061 <94114474+johnjoel061@users.noreply.github.com>
2024-10-01 11:04:45 +0800

    Update useDeleteDesignation.js

--- client/src/hooks/FormsHook/DesignationHook/useDeleteDesignation.js
+++ client/src/hooks/FormsHook/DesignationHook/useDeleteDesignation.js
@@ -11,7 +11,7 @@ const useDeleteDesignationById = () => {
     setLoading(true);
     setError(null);
     try {
-      const response = await axios.delete(`http://localhost:3000/api/designation/${id}`, {
+      const response = await axios.delete(`https://hr-management-1-baxp.onrender.com/api/designation/${id}`, {
         headers: {
           'Content-Type': 'application/json',
         }
