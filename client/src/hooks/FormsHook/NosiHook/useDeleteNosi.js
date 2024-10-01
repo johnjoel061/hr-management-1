7840619b178c09fb864e8a5a4d8ba3a0be8d452f
johnjoel061 <94114474+johnjoel061@users.noreply.github.com>
2024-10-01 11:09:57 +0800

    Update useDeleteNosi.js

--- client/src/hooks/FormsHook/NosiHook/useDeleteNosi.js
+++ client/src/hooks/FormsHook/NosiHook/useDeleteNosi.js
@@ -11,7 +11,7 @@ const useDeleteNosiById = () => {
     setLoading(true);
     setError(null);
     try {
-      const response = await axios.delete(`http://localhost:3000/api/nosi/${id}`, {
+      const response = await axios.delete(`https://hr-management-1-baxp.onrender.com/api/nosi/${id}`, {
         headers: {
           'Content-Type': 'application/json',
         }
