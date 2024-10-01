b5acfff5fd0b5bf956ad50df1488e4c1297c9b11
johnjoel061 <94114474+johnjoel061@users.noreply.github.com>
2024-10-01 11:16:47 +0800

    Update useAddSaln.js

--- client/src/hooks/FormsHook/SalnHook/useAddSaln.js
+++ client/src/hooks/FormsHook/SalnHook/useAddSaln.js
@@ -21,7 +21,7 @@ const useAddSaln = () => {
       });
 
       // Make API request to add personal data sheet
-      const response = await axios.post('http://localhost:3000/api/saln/add', formData, {
+      const response = await axios.post('https://hr-management-1-baxp.onrender.com/api/saln/add', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
