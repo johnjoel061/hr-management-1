1c3feda044d6e97521bc4090d0067e258149810c
johnjoel061 <94114474+johnjoel061@users.noreply.github.com>
2024-10-01 11:05:54 +0800

    Update useAddMarriageContract.js

--- client/src/hooks/FormsHook/MarriageContractHook/useAddMarriageContract.js
+++ client/src/hooks/FormsHook/MarriageContractHook/useAddMarriageContract.js
@@ -21,7 +21,7 @@ const useAddMarriageContract = () => {
       });
 
       // Make API request to add personal data sheet
-      const response = await axios.post('http://localhost:3000/api/marriage-contract/add', formData, {
+      const response = await axios.post('https://hr-management-1-baxp.onrender.com/api/marriage-contract/add', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
