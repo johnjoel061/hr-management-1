f427877cb48d7e725d0b096068fdf6cbf6797aef
johnjoel061 <94114474+johnjoel061@users.noreply.github.com>
2024-10-01 11:19:23 +0800

    Update useUpdateTor.js

--- client/src/hooks/FormsHook/TorHook/useUpdateTor.js
+++ client/src/hooks/FormsHook/TorHook/useUpdateTor.js
@@ -28,7 +28,7 @@ const useUpdateTorById = () => {
         }
       });
 
-      const response = await axios.put(`http://localhost:3000/api/tor/${id}`, formData, {
+      const response = await axios.put(`https://hr-management-1-baxp.onrender.com/api/tor/${id}`, formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
           Authorization: `Bearer ${localStorage.getItem('token')}`,
