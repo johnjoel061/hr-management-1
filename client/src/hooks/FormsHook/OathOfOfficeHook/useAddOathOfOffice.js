fc2d40f010f1f4a98a3a985db4e8ea738b171099
johnjoel061 <94114474+johnjoel061@users.noreply.github.com>
2024-10-01 11:11:50 +0800

    Update useAddOathOfOffice.js

--- client/src/hooks/FormsHook/OathOfOfficeHook/useAddOathOfOffice.js
+++ client/src/hooks/FormsHook/OathOfOfficeHook/useAddOathOfOffice.js
@@ -21,7 +21,7 @@ const useAddOathOfOffice = () => {
       });
 
       // Make API request to add personal data sheet
-      const response = await axios.post('http://localhost:3000/api/oath-of-office/add', formData, {
+      const response = await axios.post('https://hr-management-1-baxp.onrender.com/api/oath-of-office/add', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
