16d6b81e4c0dc3998bda28e19655609ded148555
johnjoel061 <94114474+johnjoel061@users.noreply.github.com>
2024-10-01 11:07:52 +0800

    Update useAddNbiClearance.js

--- client/src/hooks/FormsHook/NbiClearanceHook/useAddNbiClearance.js
+++ client/src/hooks/FormsHook/NbiClearanceHook/useAddNbiClearance.js
@@ -21,7 +21,7 @@ const useAddNbiClearance = () => {
       });
 
       // Make API request to add personal data sheet
-      const response = await axios.post('http://localhost:3000/api/nbi-clearance/add', formData, {
+      const response = await axios.post('https://hr-management-1-baxp.onrender.com/api/nbi-clearance/add', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
