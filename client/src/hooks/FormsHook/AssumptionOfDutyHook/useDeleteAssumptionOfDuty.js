264c652499a23a97eeb2e06babad020bc40dd6f8
johnjoel061 <94114474+johnjoel061@users.noreply.github.com>
2024-10-01 10:50:18 +0800

    Update useDeleteAssumptionOfDuty.js

--- client/src/hooks/FormsHook/AssumptionOfDutyHook/useDeleteAssumptionOfDuty.js
+++ client/src/hooks/FormsHook/AssumptionOfDutyHook/useDeleteAssumptionOfDuty.js
@@ -11,7 +11,7 @@ const useDeleteAssumptionOfDutyById = () => {
     setLoading(true);
     setError(null);
     try {
-      const response = await axios.delete(`http://localhost:3000/api/assumption-of-duty/${id}`, {
+      const response = await axios.delete(`https://hr-management-1-baxp.onrender.com/api/assumption-of-duty/${id}`, {
         headers: {
           'Content-Type': 'application/json',
         }
