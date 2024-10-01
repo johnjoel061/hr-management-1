a63c9d53db15c7be4698aa2a9e90080fadaf664c
johnjoel061 <94114474+johnjoel061@users.noreply.github.com>
2024-10-01 10:16:48 +0800

    Update useAddAward.js

--- client/src/hooks/AwardHook/useAddAward.js
+++ client/src/hooks/AwardHook/useAddAward.js
@@ -9,7 +9,7 @@ const useAddAward = () => {
   const addAward = async (userId, awData) => {
     setLoading(true);
     try {
-      const response = await axios.post(`http://localhost:3000/api/employee/awards/${userId}/add`, awData, {
+      const response = await axios.post(`https://hr-management-1-baxp.onrender.com/api/employee/awards/${userId}/add`, awData, {
         headers: {
           'Content-Type': 'application/json',
         },
