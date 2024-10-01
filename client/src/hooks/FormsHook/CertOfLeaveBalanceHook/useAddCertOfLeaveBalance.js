9825cf25e2414090161ac64457a889f37fc1a14b
johnjoel061 <94114474+johnjoel061@users.noreply.github.com>
2024-10-01 10:52:53 +0800

    Update useAddCertOfLeaveBalance.js

--- client/src/hooks/FormsHook/CertOfLeaveBalanceHook/useAddCertOfLeaveBalance.js
+++ client/src/hooks/FormsHook/CertOfLeaveBalanceHook/useAddCertOfLeaveBalance.js
@@ -21,7 +21,7 @@ const useAddCertOfLeaveBalance = () => {
       });
 
       // Make API request to add personal data sheet
-      const response = await axios.post('http://localhost:3000/api/certificate-of-leave-balance/add', formData, {
+      const response = await axios.post('https://hr-management-1-baxp.onrender.com/api/certificate-of-leave-balance/add', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
