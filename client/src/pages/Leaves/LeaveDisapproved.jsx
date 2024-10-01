5b79661cc1d088f0bbfc327f7cbb22cc1c2001b8
johnjoel061 <94114474+johnjoel061@users.noreply.github.com>
2024-10-01 11:46:31 +0800

    Update LeaveDisapproved.jsx

--- client/src/pages/Leaves/LeaveDisapproved.jsx
+++ client/src/pages/Leaves/LeaveDisapproved.jsx
@@ -60,7 +60,7 @@ const LeaveDisapproved = () => {
   );
 
   const downloadLeaveRequestPDF = (id) => {
-    window.open(`http://localhost:3000/api/employee/leave-requests/${id}/pdf`, '_blank');
+    window.open(`https://hr-management-1-baxp.onrender.com/api/employee/leave-requests/${id}/pdf`, '_blank');
   };
 
   const columns = [
