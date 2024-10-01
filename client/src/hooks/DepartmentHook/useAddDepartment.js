b99348453967d30761cc3b9703dc2228effc246d
johnjoel061 <94114474+johnjoel061@users.noreply.github.com>
2024-10-01 10:19:54 +0800

    Update useAddDepartment.js

--- client/src/hooks/DepartmentHook/useAddDepartment.js
+++ client/src/hooks/DepartmentHook/useAddDepartment.js
@@ -10,7 +10,7 @@ const useAddDepartment = () => {
     setLoading(true);
     try {
       // Make API request to add eligibility
-      const response = await axios.post('http://localhost:3000/api/department/add', departmentData, {
+      const response = await axios.post('https://hr-management-1-baxp.onrender.com/api/department/add', departmentData, {
         headers: {
           'Content-Type': 'application/json',
         },
