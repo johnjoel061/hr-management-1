5218ea3ad5488229a47168f944d5dd2095814f37
johnjoel061 <94114474+johnjoel061@users.noreply.github.com>
2024-10-01 11:14:17 +0800

    Update useDeletePersonalDataSheet.js

--- client/src/hooks/FormsHook/PersonalDataSheetHook/useDeletePersonalDataSheet.js
+++ client/src/hooks/FormsHook/PersonalDataSheetHook/useDeletePersonalDataSheet.js
@@ -11,7 +11,7 @@ const useDeletePersonalDataSheetById = () => {
     setLoading(true);
     setError(null);
     try {
-      const response = await axios.delete(`http://localhost:3000/api/personal-data-sheet/${id}`, {
+      const response = await axios.delete(`https://hr-management-1-baxp.onrender.com/api/personal-data-sheet/${id}`, {
         headers: {
           'Content-Type': 'application/json',
         }
