0cbd0cd9c18a491d167dc4c7c1da78e25db2ab39
johnjoel061 <94114474+johnjoel061@users.noreply.github.com>
2024-10-01 11:15:26 +0800

    Update useAddPositionDescriptionForm.js

--- client/src/hooks/FormsHook/PositionDescriptionFormHook/useAddPositionDescriptionForm.js
+++ client/src/hooks/FormsHook/PositionDescriptionFormHook/useAddPositionDescriptionForm.js
@@ -21,7 +21,7 @@ const useAddPositionDescriptionForm = () => {
       });
 
       // Make API request to add personal data sheet
-      const response = await axios.post('http://localhost:3000/api/position-description-form/add', formData, {
+      const response = await axios.post('https://hr-management-1-baxp.onrender.com/api/position-description-form/add', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
