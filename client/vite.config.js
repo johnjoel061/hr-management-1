742630fb171d7c67d50d1a9e5eb9734526a65758
johnjoel061 <94114474+johnjoel061@users.noreply.github.com>
2024-10-01 09:34:04 +0800

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,17 +7,4 @@ export default defineConfig({
     react(),
     commonjs(),
   ],
-
-  server: {
-    port: 3000,          // Change the port to any available port (default is 5173)
-    host: '0.0.0.0',     // Use 'localhost', '0.0.0.0', or a custom hostname
-    strictPort: true,    // If true, Vite will throw an error if the port is already in use
-    proxy: {
-      '/api': {
-        target: 'https://hr-management-1-baxp.onrender.com', // Proxy API requests to your backend server
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '') // Remove /api from the request URL
-      }
-    }
-  }
 });
