36f26803f12c5d47ed67cb83c35881b9a542a260
johnjoel061 <94114474+johnjoel061@users.noreply.github.com>
2024-10-01 10:22:02 +0800

    Update useDeleteEligibility.js

--- client/src/hooks/EligibilityHook/useDeleteEligibility.js
+++ client/src/hooks/EligibilityHook/useDeleteEligibility.js
@@ -11,7 +11,7 @@ const useDeleteEligibilityById = () => {
     setLoading(true);
     setError(null);
     try {
-      const response = await axios.delete(`http://localhost:3000/api/eligibility/${id}`, {
+      const response = await axios.delete(`https://hr-management-1-baxp.onrender.com/api/eligibility/${id}`, {
         headers: {
           'Content-Type': 'application/json',
         }
