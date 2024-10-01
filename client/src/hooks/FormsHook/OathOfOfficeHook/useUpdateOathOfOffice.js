246ada310a27b44879f6073e277e7f15c8a1ddfe
johnjoel061 <94114474+johnjoel061@users.noreply.github.com>
2024-10-01 11:13:20 +0800

    Update useUpdateOathOfOffice.js

--- client/src/hooks/FormsHook/OathOfOfficeHook/useUpdateOathOfOffice.js
+++ client/src/hooks/FormsHook/OathOfOfficeHook/useUpdateOathOfOffice.js
@@ -28,7 +28,7 @@ const useUpdateOathOfOfficeById = () => {
         }
       });
 
-      const response = await axios.put(`http://localhost:3000/api/oath-of-office/${id}`, formData, {
+      const response = await axios.put(`https://hr-management-1-baxp.onrender.com/api/oath-of-office/${id}`, formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
           Authorization: `Bearer ${localStorage.getItem('token')}`,
