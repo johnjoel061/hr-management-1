d1000cf82570719a734a1655eb0e881322d7d01d
johnjoel061 <94114474+johnjoel061@users.noreply.github.com>
2024-10-01 10:19:00 +0800

    Update useDeleteCalendar.js

--- client/src/hooks/CalendarHook/useDeleteCalendar.js
+++ client/src/hooks/CalendarHook/useDeleteCalendar.js
@@ -11,7 +11,7 @@ const useDeleteCalendar = () => {
     setLoading(true);
     setError(null);
     try {
-      const response = await axios.delete(`http://localhost:3000/api/calendar/${id}`, {
+      const response = await axios.delete(`https://hr-management-1-baxp.onrender.com/api/calendar/${id}`, {
         headers: {
           'Content-Type': 'application/json',
         }
