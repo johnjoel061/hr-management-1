df5b7005096251a539d4b24f90a06a0b1fd9b1ce
johnjoel061 <94114474+johnjoel061@users.noreply.github.com>
2024-10-01 10:53:15 +0800

    Update useDeleteCertOfLeaveBalance.js

--- client/src/hooks/FormsHook/CertOfLeaveBalanceHook/useDeleteCertOfLeaveBalance.js
+++ client/src/hooks/FormsHook/CertOfLeaveBalanceHook/useDeleteCertOfLeaveBalance.js
@@ -11,7 +11,7 @@ const useDeleteCertOfLeaveBalanceById = () => {
     setLoading(true);
     setError(null);
     try {
-      const response = await axios.delete(`http://localhost:3000/api/certificate-of-leave-balance/${id}`, {
+      const response = await axios.delete(`https://hr-management-1-baxp.onrender.com/api/certificate-of-leave-balance/${id}`, {
         headers: {
           'Content-Type': 'application/json',
         }
