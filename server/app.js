203641dadcffee8d058c80e2f510c646e0da1bd6
johnjoel061 <94114474+johnjoel061@users.noreply.github.com>
2025-04-26 12:24:12 +0800

    Update app.js

--- server/app.js
+++ server/app.js
@@ -178,7 +178,7 @@ if (process.env.NODE_ENV === "development") {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, 'public')));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'build/index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
   });
 }
 
